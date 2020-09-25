fca773a356ac1d5f2e68e07b254c9f3d033c7ac6
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2020-09-25 16:30:40 -0300

    Começando a criar o FRONT

---
+++ frontend/webpack.config.js
@@ -0,0 +1,10 @@
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './app.js'
+    }
+}

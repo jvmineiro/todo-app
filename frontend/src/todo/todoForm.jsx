116d9333c2681a82a0fd08d6627b982ac6ca6f6d
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2020-10-08 13:54:40 -0300

    FINALIZADO

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,23 +2,34 @@ import React from 'react'
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-export default props => (
-    <div role="from" className="todoForm">
+export default props => {
+    const keyHnandler = (e) => {
+        if (e.key === 'Enter') {
+            e.shiftKey ? props.handlerSearch() : props.handleAdd()
+        } else if(e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
 
-        <Grid cols="12 9 10">
-            <input id="description" className="form-control" 
-                placeholder="Adicione uma tarefa" type="text"
-                onChange={props.handleChange}
-                value={props.description} />
-        </Grid>
-
-       <Grid cols="12 3 2">
-           <IconButton style="primary" icon="plus"
-                onClick={props.handleAdd}></IconButton>
-            <IconButton style="info" icon="search" 
-                onClick={props.handleSearch}></IconButton>
-           <IconButton style="default" icon="close"
-                onClick={props.handleClear}></IconButton>    
-        </Grid>
-    </div>
-)
+    return (
+        <div role="from" className="todoForm">
+    
+            <Grid cols="12 9 10">
+                <input id="description" className="form-control" 
+                    placeholder="Adicione uma tarefa" type="text"
+                    onChange={props.handleChange}
+                    onKeyUp={keyHnandler}
+                    value={props.description} />
+            </Grid>
+    
+            <Grid cols="12 3 2">
+                <IconButton style="primary" icon="plus"
+                    onClick={props.handleAdd}></IconButton>
+                <IconButton style="info" icon="search" 
+                    onClick={props.handleSearch}></IconButton>
+                <IconButton style="default" icon="close"
+                    onClick={props.handleClear}></IconButton>    
+            </Grid>
+        </div>
+    )
+}

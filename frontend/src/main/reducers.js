375c05701820021a063dd9234d4a217c7ea122fb
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2020-12-16 19:26:40 -0300

    action creator and changeDescription

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,22 +1,8 @@
 import { combineReducers } from 'redux';
+import todoReducer from '../todo/todoReducer'
 
 const rootReducer = combineReducers({
-    todo: () => ({
-        description: 'Ler Livro',
-        list: [{
-            _id: 1,
-            description: 'Pagar fatura do cartao',
-            done: true
-        },{
-            _id: 2,
-            description: 'Reunião com a equipe as 10:00',
-            done: false
-        },{
-            _id: 3,
-            description: 'Consulta medica na terça depois do almoço',
-            done: false
-        }]
-    })
+    todo: todoReducer
 })
 
 export default rootReducer

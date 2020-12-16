5d4ce1ae8b3319f2d57989b2e4fb1cf6be71f88e
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2020-12-16 09:26:20 -0300

    Reducer configurado

---
+++ frontend/src/main/reducers.js
@@ -0,0 +1,22 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    todo: () => ({
+        description: 'Ler Livro',
+        list: [{
+            _id: 1,
+            description: 'Pagar fatura do cartao',
+            done: true
+        },{
+            _id: 2,
+            description: 'Reunião com a equipe as 10:00',
+            done: false
+        },{
+            _id: 3,
+            description: 'Consulta medica na terça depois do almoço',
+            done: false
+        }]
+    })
+})
+
+export default rootReducer
